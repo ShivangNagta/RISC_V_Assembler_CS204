aa1838c0734d0f6494559bed12d87a69344965ac
ShivangNagta <shivangnag@gmail.com>
2025-04-11 21:46:05 +0530

    remove fileio

--- backend/include/assembler.h
+++ backend/include/assembler.h
@@ -5,15 +5,14 @@ Uses two passes to convert the assembly to machine code
 #pragma once
 
 #include "parser.h"
-#include "io.h"
 #include "symbol_table.h"
 #include "memory.h"
+#include "constants.h"
 
 class Assembler {
     SymbolTable symbols;
     Memory& memory;  
     Parser parser;   
-    FileIO fileIO;
     
 public:
     Assembler(Memory &memory) : parser(memory), memory(memory) {} ;
