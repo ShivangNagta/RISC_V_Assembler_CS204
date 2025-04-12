4e6fa64719d4f15e50beae46bf17083f95c27836
ShivangNagta <shivangnag@gmail.com>
2025-04-12 17:32:45 +0530

    server assemble endpoint setup

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,29 +1,71 @@
-const express = require("express");
-const cors = require("cors");
-const fs = require("fs");
-const { exec } = require("child_process");
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import { spawn } from 'child_process'
+import { v4 as uuidv4 } from 'uuid';
+import { fileURLToPath } from 'url';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+let users = new Map()
+
 app.post("/assemble", (req, res) => {
-    const { code } = req.body;
-    const inputFile = "input.asm";
-    const outputFile = "output.mc";
+    let { id, input_code } = req.body;
+    let session;
+    
+    if (!id || !users.has(id)) {
+        id = uuidv4();
+        console.log("hello")
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        const assemblerPath = path.resolve(__dirname, "../main");
 
-    // Save input assembly to a file
-    fs.writeFileSync(inputFile, code);
+        const child = spawn(assemblerPath);
+        session = { child, output: "" };
+        users.set(id, session);
 
-    // Call the assembler binary
-    exec(`../main ${inputFile} ${outputFile}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error("Assembler Error:", stderr);
-            return res.status(500).json({ error: stderr });
-        }
-        const output = fs.readFileSync(outputFile, "utf8");
-        res.json({ machineCode: output });
-    });
+        child.stdout.on("data", (data) => {
+            session.output += data.toString();
+        });
+
+        child.stderr.on("data", (data) => {
+            session.output += data.toString();
+            res.status(400).json({ error: session.output})
+        });
+    } else {
+        session = users.get(id);
+    }
+
+    session.output = ""; 
+    session.child.stdin.write("assemble\n");
+    // const cod = {
+    //     "input_code": "add x1, x2, x3\naddi x3, x4, 10\n"
+    // }
+    const newC = {
+        "input_code": input_code
+    }
+    // console.log(JSON.stringify(newC))
+    session.child.stdin.write(JSON.stringify(newC)+"\n");
+
+    setTimeout(() => {
+        const assembledJSON = JSON.parse(session.output)
+        res.json({ machine_code: assembledJSON.machine_code, data_segment: assembledJSON.data_segment, id });
+    }, 100);
 });
+
+
+app.post("/reset", (req,res) => {
+    
+})
+
+app.post("/step", (req,res) => {
+
+})
+
+app.post("/run", (req,res) => {
+
+})
 
 app.listen(3000, () => console.log("Server running on port 3000"));
